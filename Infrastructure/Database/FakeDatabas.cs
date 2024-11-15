423963d63a5a0c5059cc59b6e8282c4215bed5e0
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2024-11-15 14:11:30 +0100

    fixat problem med test för add new book

--- Infrastructure/Database/FakeDatabas.cs
+++ Infrastructure/Database/FakeDatabas.cs
@@ -9,9 +9,21 @@ namespace Infrastructure.Database
 {
     public class FakeDatabas
     {
-        List<Book> allBooksFromDb = new List<Book>
-        {
+        public List<Book>Books {  get { return allBooksFromDb; } set { allBooksFromDb = value; } }
 
+        private static List<Book> allBooksFromDb = new List<Book>
+        {
+            new Book (1, "BranislavBook1", "Description1"),
+            new Book (2, "BranislavBook2", "Description2"),
+            new Book (3, "BranislavBook3", "Description3"),
+            new Book (4, "BranislavBook4", "Description4"),
+            new Book (5, "BranislavBook5", "Description5"),
         };
+
+        public Book AddNewBook(Book book)
+        {
+            allBooksFromDb.Add(book);
+            return book;
+        }
     }
 }
