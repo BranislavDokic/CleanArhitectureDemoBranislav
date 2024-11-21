a1c432334311d2fb646b09ef909de143868616d9
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2024-11-21 21:36:38 +0100

    api implementerat

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Books.Commands.CreateBook;
+using Domain;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+
+            
+
+            return services;
+        }
+    }
+}
