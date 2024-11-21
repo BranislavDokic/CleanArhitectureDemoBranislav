925c2b0609d481ad555fafec4fa15b491b3ead2f
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2024-11-22 00:30:20 +0100

    api now works and i implemented deletebook

---
+++ Application/Books/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Books.Commands.DeleteBook
+{
+    public class DeleteBookCommand : IRequest<bool>
+    {
+        public int BookId { get; }
+
+        public DeleteBookCommand(int bookId)
+        {
+            BookId = bookId;
+        }
+    }
+}
