5fd0290a06915157ae4918b4cbc1fcf9fa720afd
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2024-11-22 13:03:16 +0100

    implemented get all queri

---
+++ Application/Books/Queries/GetAllBook/GetAllBookQueri.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Books.Queries.GetAllBook
+{
+    public class GetAllBooksQuery : IRequest<List<Book>>
+    {
+    }
+}
---
+++ Application/Books/Queries/GetAllBook/GetAllQuerihandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Books.Queries.GetAllBook
+{
+    public class GetAllBooksQueryHandler : IRequestHandler<GetAllBooksQuery, List<Book>>
+    {
+        private readonly FakeDatabas _database;
+
+        public GetAllBooksQueryHandler(FakeDatabas database)
+        {
+            _database = database;
+        }
+
+        public Task<List<Book>> Handle(GetAllBooksQuery request, CancellationToken cancellationToken)
+        {
+            
+            var books = _database.Books.ToList();
+
+          
+            return Task.FromResult(books);
+        }
+    }
+}
